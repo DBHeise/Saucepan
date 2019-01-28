92c73d1389529a245e5a860219392ab9343e0366
DBHeise <david@heiseink.com>
2019-01-28 10:15:18 -0500

    adding config option to save unjuiced to seperate file

--- config.go
+++ config.go
@@ -20,6 +20,7 @@ type esconfig struct {
 }
 type configuration struct {
 	MoveAfterProcessed bool      `json:"MoveAfterProcessed"`
+	SavedUnjuiced      bool      `json:"SaveUnjuiced"`
 	WatchFolder        string    `json:"WatchFolder"`
 	DoneFolder         string    `json:"DoneFolder"`
 	WaitInterval       int       `json:"WaitInterval"`
